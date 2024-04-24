5006c8ca3fbb48325fb0837fbd27aa84e0537a57
babulalmahto <babulalmatho4394@gmail.com>
2024-04-24 10:49:35 +0530

    folder created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
 
-app.listen(8080, ()=>{
-    console.log('Server started');
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const MODE = process.env.MODE || PRO;
+const localhost = process.env.localhost || '127.0.0.2';
+
+app.listen(PORT, localhost, () => {
+    console.log(`Server started at http://${localhost}:${PORT} in ${MODE} mode`);
 });
